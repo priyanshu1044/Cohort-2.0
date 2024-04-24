50d2d5535d1de643b8669bd0c253fa8af237b45e
priyanshupatel1044@gmail.com <priyanshupatel1044@gmail.com>
2024-04-24 12:59:47 +0530

    add user into users table

--- week10-databases/src/index.ts
+++ week10-databases/src/index.ts
@@ -1,21 +1,24 @@
-import { Client } from 'pg'
- 
-const client = new Client({
-  connectionString: "postgresql://postgres:mysecretpassword@localhost/postgres"
-})
+import { Client } from 'pg';
 
-async function createUsersTable() {
-    await client.connect()
-    const result = await client.query(`
-        CREATE TABLE users (
-            id SERIAL PRIMARY KEY,
-            username VARCHAR(50) UNIQUE NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-        );
-    `)
-    console.log(result)
+async function insertData() {
+  const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    user: 'postgres',
+    password: 'mysecretpassword',
+  });
+
+  try {
+    await client.connect(); 
+    const insertQuery = "INSERT INTO users (username, email, password) VALUES ('username2', 'user3@example.com', 'user_password');";
+    const res = await client.query(insertQuery);
+    console.log('Insertion success:', res); 
+  } catch (err) {
+    console.error('Error during the insertion:', err);
+  } finally {
+    await client.end(); 
+  }
 }
 
-createUsersTable();
+insertData();
