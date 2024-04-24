7ddc401b7a577dc511cdb4d8e54840ae1ee1e362
priyanshupatel1044@gmail.com <priyanshupatel1044@gmail.com>
2024-04-25 01:23:56 +0530

    Add updateUser function to update user data

--- week10-prisma-db/src/index.ts
+++ week10-prisma-db/src/index.ts
@@ -2,19 +2,24 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function insertUser(username: string, password: string, firstName: string, lastName: string) {
-  const res = await prisma.user.create({
-    data: {
-        username,
-        password,
-        firstName,
-        lastName
-    },select: {
-        id: true,
-        username: true,
-        }
-  })
+interface UpdateParams {
+    firstName: string;
+    lastName: string;
+}
+
+async function updateUser(username: string, {
+    firstName,
+    lastName
+}: UpdateParams) {
+  const res=await prisma.user.update({
+    where: { username },
+    data: { firstName, lastName }
+  });
+
   console.log(res);
 }
 
-insertUser("admin1", "123456", "Priyanshu", "Patel")
+updateUser("admin1", {
+  firstName: "Luffy",
+  lastName: "Monkey D"
+})
