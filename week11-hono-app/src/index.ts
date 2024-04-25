3068be7fcbe49766159bb99ff013941d352b4089
priyanshupatel1044@gmail.com <priyanshupatel1044@gmail.com>
2024-04-25 18:12:46 +0530

    Added authMiddleware in HONO app

--- week11-hono-app/src/index.ts
+++ week11-hono-app/src/index.ts
@@ -2,7 +2,14 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.post('/', async (c) => {
+const authMiddleware = async (c:any, next:any) => {
+  if (c.req.header("Authorization")) {
+    await next()
+  }
+  return c.status(401).text('Unauthorized')
+}
+
+app.post('/',authMiddleware, async (c) => {
   const body = await c.req.json()
   console.log(body);
   console.log(c.req.header("Authorization"));
