8ba98a45dc92c1e7b5dfd68efd2fc430e3089633
priyanshupatel1044@gmail.com <priyanshupatel1044@gmail.com>
2024-05-31 12:00:42 +0530

    Added backend

--- week14-nextjs/src/app/api/user/route.ts
+++ week14-nextjs/src/app/api/user/route.ts
@@ -1,16 +1,25 @@
-import { NextRequest } from "next/server"
+import { NextRequest,NextResponse } from "next/server"
+import { PrismaClient } from "@prisma/client";
 
-export async function GET() {
-    return Response.json({ 
-        name: "Priyanshu", email: "priyanshu@gmail.com" 
-    })
-}
+const client = new PrismaClient()
 
-export async function POST(req:NextRequest){
-    const body=await req.json();
-    console.log(body)
 
-    return Response.json({ 
-        message:"You are logged in!"
-    })
+// export async function GET() {
+//     const user = await client.user.findFirst({});
+//     return Response.json({ name: user?.username, email: user?.username })
+// }
+
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    // should add zod validation here
+    const user = await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    });
+
+    console.log(user.id);
+
+    return NextResponse.json({ message: "Signed up" });
 }
